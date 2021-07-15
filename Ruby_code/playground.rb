2f3481af8e4656a623d2f5c9fb3f76043ee76ae5
Walter M <waltermccone88@gmail.com>
2021-07-15 16:51:04 -0500

    Practice string interpolation

--- Ruby_code/playground.rb
+++ Ruby_code/playground.rb
@@ -1,3 +1,11 @@
 hello = "hello world!"
 
 puts hello
+
+#concat
+first_name = "Walter"
+last_name = "McCone"
+
+puts first_name+ " " + last_name
+
+puts "my first name is #{first_name} and my last name is #{last_name}"
