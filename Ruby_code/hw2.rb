a948ac67822fc836acf0193abe9c35a3aa175fb1
Walter M <waltermccone88@gmail.com>
2021-07-21 10:21:20 -0500

    added methods to hw 2

--- Ruby_code/hw2.rb
+++ Ruby_code/hw2.rb
@@ -1,3 +1,26 @@
+def multiply(first_num, second_num)
+    first_number.to_f * second_number.to_f
+end
+
+def divide(first_num, second_num)
+    first_number.to_f * second_number.to_f
+end
+
+def add(first_num, second_num)
+    first_number.to_f * second_number.to_f
+end
+
+def subtract(first_num, second_num)
+    first_number.to_f - second_number.to_f
+end
+
+def modulo(first_num, second_num)
+    first_number.to_f % second_number.to_f
+end
+
+
+
+
 puts "Basic Calculations"
 20.times {print "-"}
 puts
@@ -7,8 +30,8 @@ puts "Please enter your second number"
 second_number = gets.chomp
 puts "Thank you, calculating now.."
 20.times {print "-"}
-puts "The first number multiplied by the second number is: #{first_number.to_f * second_number.to_f}"
-puts "The first number divided by the second number is: #{first_number.to_f / second_number.to_f}"
-puts "The first number added to the second number is: #{first_number.to_f + second_number.to_f}"
-puts "The second number subtracted from the first number is: #{first_number.to_f - second_number.to_f}"
-puts "The remainder when the first number is divided by the second number: #{first_number.to_f % second_number.to_f}"
+puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
+puts "The first number divided by the second number is: #{divide(first_number, second_number)}"
+puts "The first number added to the second number is: #{add(first_number, second_number)}"
+puts "The second number subtracted from the first number is: #{subtract(first_number, second_number)}"
+puts "The remainder when the first number is divided by the second number: #{modulo(first_number, second_number)}"
