5e116e7ebd299f7381ce90530fa4b3c7b2358280
Walter M <waltermccone88@gmail.com>
2021-07-29 16:34:36 -0500

    create my first ruby class for Employee

---
+++ Ruby_code/directory.rb
@@ -0,0 +1,14 @@
+class Employee
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def to_s
+    "First Name: #{@first_name}, Last Name: #{@last_name}, Username: #{@username}, Email Address: #{@email}"
+  end
