a8699d5bd0fd62ec5de3629d6cd242d5359dd6c1
Walter M <waltermccone88@gmail.com>
2021-07-29 16:41:21 -0500

    added my bcryptauthenticator to my employee class

--- Ruby_code/bcryptauth.rb
+++ Ruby_code/bcryptauth.rb
@@ -4,22 +4,22 @@ module Bauth
   puts "BAuth Activated"
 
   
-  def self.create_hash_digest(password)
+  def create_hash_digest(password)
     BCrypt::Password.create(password)
   end
   
-  def self.verify_hash_digest(password)
+  def verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
   
-  def self.create_secure_users(list_of_users)
+  def create_secure_users(list_of_users)
     list_of_users.each do |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     end
     list_of_users
   end
 
-  def self.authenticate_user(username, password, list_of_users)
+  def authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
         print "You are now logged in as #{user_record[:username]} "
--- Ruby_code/directory.rb
+++ Ruby_code/directory.rb
@@ -1,4 +1,6 @@
+require_relative 'bcryptauth'
 class Employee
+include Bauth
   attr_accessor :first_name, :last_name, :email, :username, :password
 
   def initialize(firstname, lastname, username, email, password)
@@ -13,3 +15,9 @@ class Employee
     "First Name: #{@first_name}, Last Name: #{@last_name}, Username: #{@username}, Email Address: #{@email}"
   end
 end
+
+walter = Employee.new("Walter", "McCone", "wmccone", "walter@gmail.com", "password123")
+
+new_password = walter.create_hash_digest(walter.password)
+
+puts new_password
